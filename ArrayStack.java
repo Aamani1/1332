3b4bc14104be2e6a8281e06f498f9e550b773884
Amani Konduru <akonduru2@student.gsu.edu>
2018-02-28 21:52:47 -0500

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -4,8 +4,6 @@ import java.util.NoSuchElementException;
  * Your implementation of an array-backed stack.
  *
  * @author Amani Konduru
- * @userid akonduru3
- * @GTID 903163197
  * @version 1.0
  */
 public class ArrayStack<T> implements StackInterface<T> {
